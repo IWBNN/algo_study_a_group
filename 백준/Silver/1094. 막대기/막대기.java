fbe97118bba22f71162d2bdc6225fda411951a6a
geon <138088207+IWBNN@users.noreply.github.com>
2024-01-29 16:12:08 +0900

    [Silver V] Title: 막대기, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int cm = Integer.parseInt(br.readLine());
+        int stick = 64;
+        int count = 0;
+        while(cm != 0){
+            if (cm < stick){
+                stick = stick/2;
+            }
+            else{
+                cm = cm - stick;
+                count++;
+                if(cm==0) {
+                    break;
+                }
+            }
+        }
+        bw.write(count + "");
+        bw.flush();
+        bw.close();
+    }
+}
