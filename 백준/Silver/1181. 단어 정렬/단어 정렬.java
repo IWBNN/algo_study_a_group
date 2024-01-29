3edb13de4503835a8bb2360d6ecb9743a9ca7396
geon <138088207+IWBNN@users.noreply.github.com>
2024-01-29 16:18:45 +0900

    [Silver V] Title: 단어 정렬, Time: 892 ms, Memory: 40256 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main { //1181
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = Integer.parseInt(scanner.nextLine());
+        String[] TextA = new String[T];
+
+        for (int i = 0; i < T; i++) {
+            TextA[i] = scanner.next();
+        }
+
+        // 중복 제거
+        HashSet<String> hashSet = new HashSet<>(Arrays.asList(TextA));
+        String[] noDupTextA = hashSet.toArray(new String[0]);
+
+        // 정렬
+        Arrays.sort(noDupTextA, Comparator.comparingInt(String::length).thenComparing(Comparator.naturalOrder()));
+
+        // 출력
+        for (String word : noDupTextA) {
+            System.out.println(word);
+        }
+    }
+}
