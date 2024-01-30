c0e8e3ae918d523072a5d26ff5a26f73d56ee357
geon <138088207+IWBNN@users.noreply.github.com>
2024-01-30 16:32:50 +0900

    [Silver V] Title: 소수, Time: 136 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException { // 1312
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//        double T = (21.0 / 5.0);
+//        System.out.println(T);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int sum = 0;
+
+        for(int i =0;i<c;i++){
+            a = (a%b) * 10;
+            sum = a/b;
+        }
+        System.out.println(sum);
+    }
+}
