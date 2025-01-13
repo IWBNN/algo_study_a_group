cd48d2506d59a11c573852c2b3835330d93ee3fd
geon <138088207+IWBNN@users.noreply.github.com>
2025-01-13 16:31:21 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 12.59 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,37 +1,39 @@
 import java.util.*;
 
 class Solution {
+    class Truck {
+        int weight;
+        int exitTime;    // 다리를 빠져나가는 시간
+        
+        Truck(int weight, int exitTime) {
+            this.weight = weight;
+            this.exitTime = exitTime;
+        }
+    }
+    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> bridge = new LinkedList<>();
-        int time = 0;
+        Queue<Truck> bridge = new LinkedList<>();
         int currentWeight = 0;
+        int time = 0;
         int truckIndex = 0;
         
-        // 다리의 길이만큼 0으로 채우기 (빈 공간 표시)
-        for (int i = 0; i < bridge_length; i++) {
-            bridge.offer(0);
-        }
-        
-        // 모든 트럭이 다리를 건널 때까지
-        while (truckIndex < truck_weights.length) {
-            // 시간 증가
-            time++;
-            
-            // 다리에서 나가는 트럭의 무게를 전체 무게에서 빼기
-            currentWeight -= bridge.poll();
+        while(truckIndex < truck_weights.length || !bridge.isEmpty()) {
+            // 현재 시간에 다리를 빠져나가는 트럭 처리
+            if(!bridge.isEmpty() && bridge.peek().exitTime == time) {
+                currentWeight -= bridge.poll().weight;
+            }
             
             // 새 트럭이 다리에 올라갈 수 있는지 확인
-            if (currentWeight + truck_weights[truckIndex] <= weight) {
-                bridge.offer(truck_weights[truckIndex]);
+            if(truckIndex < truck_weights.length && 
+               currentWeight + truck_weights[truckIndex] <= weight) {
                 currentWeight += truck_weights[truckIndex];
+                bridge.offer(new Truck(truck_weights[truckIndex], 
+                                     time + bridge_length));
                 truckIndex++;
-            } else {
-                bridge.offer(0);  // 트럭이 올라갈 수 없으면 빈 공간 추가
             }
+            
+            time++;
         }
-        
-        // 마지막 트럭이 다리를 건너는 시간 추가
-        time += bridge_length;
         
         return time;
     }
