d1358347720b55afbe86ddfa1e4a11c470d7001d
geon <138088207+IWBNN@users.noreply.github.com>
2025-03-10 15:26:48 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] selected;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        selected = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        backTracking(0, 0);
+        System.out.println(sb);
+    }
+
+    static void backTracking(int depth, int start) {
+        if (depth == M) {
+            for (int j = 0; j < M; j++) {
+                sb.append(selected[j]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                selected[depth] = arr[i];
+                backTracking(depth + 1, i + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
